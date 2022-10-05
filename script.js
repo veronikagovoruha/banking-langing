49e8de1915ffece5529f37ec672c6d2f0b6f72c8
Veronika <veronikagovoruha@gmail.com>
2022-10-05 19:46:55 +0300

    add slick slider

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function (){
+    $('.slider__list').slick({
+        dots: true,
+        speed: 1000,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 2,
+    });
+
+    $('.image__list').slick({
+        arrow: false,
+        dots: true,
+        speed: 1000,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    })
+})
