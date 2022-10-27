853101ba538df38b0461080c8c12061ae4d95900
Veronika <veronikagovoruha@gmail.com>
2022-10-27 15:37:28 +0300

    add tablet and mobile 5 section

--- script.js
+++ script.js
@@ -7,12 +7,22 @@ $(function (){
         slidesToScroll: 2,
     });
 
-    // $('.image__list').slick({
-    //     arrow: false,
-    //     dots: true,
-    //     speed: 1000,
-    //     infinite: true,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    // })
+    $('.image__list').slick({
+        vertical: true,
+        arrows: false,
+        dots: true,
+        speed: 1000,
+        // fade: true,
+        // autoplay: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    })
+})
+
+$(document).ready(function (){
+    $('.header__burger').click(function (event){
+        $('.header__burger, .nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
 })
